825e8557e84b888e829a947ae95aa040fa6399e0
stevewolverine <78geri@gmail.com>
2025-06-15 11:48:45 -0600

    Update etl_firestore.py

--- etl_firestore.py
+++ etl_firestore.py
@@ -1,6 +1,7 @@
 import os, json, io, tempfile, hashlib, time, asyncio
 from dbfread import DBF
 from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseDownload
 from google.oauth2 import service_account
 import firebase_admin
 from firebase_admin import credentials, firestore
